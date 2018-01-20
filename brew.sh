3d8195dabb0511bfff60d160e24bdb8e28900673
Srinivas Dhanwada <dhanwads@gmail.com>
2018-01-20 00:39:25 -0600

    Add a list of all brew packages to install

---
+++ brew.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# install command line tools using Homebrew.
+
+# Make sure brew is updated
+brew update
+brew upgrade
+
+brew install automake
+brew install clang-format
+brew tap dart-lang/dart
+brew install dart --with-content-shell --with-dartium
+brew install dirmngr
+brew install ffmpeg
+brew install gdb
+brew install gifsicle
+brew install git-lfs
+brew install gnupg
+brew install gpg-agent
+brew install heroku/brew/heroku
+brew install htop
+brew install icu4c
+brew install imagemagick
+brew install libusb-compat
+brew install libyaml
+brew install maven
+brew install mongodb
+brew install mysql
+brew install ncdu
+brew install netpbm
+brew install nvm
+brew install pinentry-mac
+brew install pkg-config
+brew install python3
+brew install wget
+brew install xmlto
+
